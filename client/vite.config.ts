90ec5d71f9683c040412bc9cda7a02e4052f917e
aleksandre <faleqs33@gmail.com>
2025-09-15 23:51:23 +0400

    fifth try

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
 
-export default defineConfig({
-  base: '/ShippingPlatform/',   
-  server: { open: '/login' },   
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/ShippingPlatform/' : '/',
+  server: { open: '/login' },
   plugins: [react(), WindiCSS()],
-});
+}));
