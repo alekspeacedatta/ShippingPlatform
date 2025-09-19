35979e71981d33c6336ee34cc98324185d8a17e6
aleksandre <faleqs33@gmail.com>
2025-09-19 18:48:44 +0400

    minor

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
 
-export default defineConfig(() => ({
-  base: '/ShippingPlatform/',               
-  build: { outDir: 'docs', emptyOutDir: true },
-  plugins: [react(), WindiCSS()],
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/ShippingPlatform/' : '/',
   server: { open: '/login' },
+  plugins: [react(), WindiCSS()],
 }));
