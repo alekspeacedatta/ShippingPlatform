fff91881c893f5aac4969e15ae302f0240fb3bc9
aleksandre <faleqs33@gmail.com>
2025-09-23 14:56:08 +0400

    minor

--- client/src/services/ChatService.ts
+++ client/src/services/ChatService.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from "../types/Types";
 
 export class Chat {
     static async setMessage (message: { companyId: string, userId: string, sentMessage: string, date: Date }) {
-        const res = await fetch(`${BASE_URL}/api/set-message`, {
+        const res = await fetch(`${BASE_URL}/api/messages/set-message`, {
             method: "POST",
             headers: {
                 'Content-type': 'application/json'
@@ -14,7 +14,7 @@ export class Chat {
         return res.json();
     }
     static async getMessages (id: string) {
-        const res = await fetch(`${BASE_URL}/api/get-message?id=${encodeURIComponent(id)}`, {
+        const res = await fetch(`${BASE_URL}/api/messages/get-message?id=${encodeURIComponent(id)}`, {
             method: "GET", 
         })
         if(!res) throw new Error("Error while doing this");
